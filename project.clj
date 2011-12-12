fee1c9cd252f18f82cc072d4e4273904b175e9dd
Myles Megyesi <mylesmegyesi@me.com>
2011-12-12 15:27:27 -0600

    added group id

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tic-tac-toe-web-server "0.1-SNAPSHOT"
+(defproject org.clojars.mylesmegyesi/tic-tac-toe-web-server "0.1-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.clojars.mylesmegyesi/tic-tac-toe "0.1-SNAPSHOT"]
